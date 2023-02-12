bb88beee74060e19cfa3877fe55404793fe351c1
Aromal Nambiar <aromalndr@gmail.com>
2023-02-12 17:27:13 +0530

    update no.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
-drawer = document.querySelector(".drawer")
-drawer.onclick = function () {
-    navBar = document.querySelector(".right_head")
-    navBar.classList.toggle("active")
-}
+const hoverLink = document.querySelector("#our_way_hover1");
+const hoverText = document.querySelector("#para1");
+
+hoverLink.addEventListener("mouseenter", function() {
+  hoverText.style.display = "block";
+});
+
+hoverLink.addEventListener("mouseleave", function() {
+  hoverText.style.display = "none";
+});
+
+// drawer = document.querySelector(".drawer")
+// drawer.onclick = function () {
+//     navBar = document.querySelector(".right_head")
+//     navBar.classList.toggle("active")
+// }
 
 
 
